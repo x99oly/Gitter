1b318b711dc5dbbbe406f37427df945b029fca90
Sameu Araujo <oliveira.samuel.edu@gmail.com>
2025-04-06 19:03:34 -0300

    Git conectado com sucesso!

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('Gitter is on!')
+import { Octokit, App } from "octokit"
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+async function Main()
+{
+    const _gitClient = new Octokit( { auth: String(process.env.GITHUB_KEY)})
+
+    const {
+        data: { login },
+    } = await _gitClient.rest.users.getAuthenticated()
+
+    console.log(`Hello, ${login}`)
+}
+
+Main()
